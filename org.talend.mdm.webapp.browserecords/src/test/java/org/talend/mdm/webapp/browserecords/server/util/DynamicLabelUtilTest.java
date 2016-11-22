d40ccfd6e4d05e789cdf42314d10d7de6016dd9c
yjli-talend <yjli@talend.com>
2016-11-22 14:11:30 +0800

    TMDM-10164 Fixed compile error.

--- org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/DynamicLabelUtilTest.java
+++ org.talend.mdm.webapp.browserecords/src/test/java/org/talend/mdm/webapp/browserecords/server/util/DynamicLabelUtilTest.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 

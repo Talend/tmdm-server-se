975dcaf8362a02d650cfaaa7ca1d710735ca4bc5
Ken Kang <zfkang@talend.com>
2017-01-16 18:15:46 +0800

    Revert "modify the junit case class name"
    This reverts commit 00c34df10d766ecbb897424bfa26530bfb214a8d.

--- org.talend.mdm.core/test/com/amalto/core/server/MDMTransactionManagerTestCase.java
+++ org.talend.mdm.core/test/com/amalto/core/server/MDMTransactionManagerTestCase.java
@@ -19,7 +19,7 @@ import com.amalto.core.storage.transaction.Transaction;
 import com.amalto.core.storage.transaction.Transaction.Lifetime;
 
 
-public class MDMTransactionManagerTest {
+public class MDMTransactionManagerTestCase {
     
     private MDMTransactionManager transactionManager;
     

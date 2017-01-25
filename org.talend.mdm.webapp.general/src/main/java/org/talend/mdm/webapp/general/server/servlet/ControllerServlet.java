5fe87b8c105d632f7224c2ea0b754732e2ed96b3
Ken Kang <zfkang@talend.com>
2017-01-25 15:50:41 +0800

    TMDM-10028: allow copy of a greyed field value

--- org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/server/servlet/ControllerServlet.java
+++ org.talend.mdm.webapp.general/src/main/java/org/talend/mdm/webapp/general/server/servlet/ControllerServlet.java
@@ -115,7 +115,7 @@ public class ControllerServlet extends HttpServlet {
         if (customHtml != null) {
             html.append(customHtml);
         }
-        html.append("<script type='text/javascript'>document.onselectstart = new Function('event.returnValue=true;');</script>\n"); //$NON-NLS-1$
+        html.append("<script type='text/javascript' language='javascript' src='secure/js/chromeCopy.js'></script>\n"); //$NON-NLS-1$
         html.append("</head>");
         html.append("<body style=\"-moz-user-select: text\">");
         html.append("<div id='loading'>");

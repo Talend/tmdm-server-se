0d36b991f2fd9e6d03750502676ec772cb2841c4
Ken Kang <zfkang@talend.com>
2017-06-22 10:53:42 +0800

    TMDM-10993: Unable to redeploy a DataModel after adding a reusable complex type Element to an Entity

--- org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/storage/hibernate/LiquibaseSchemaAdapterTest.java
+++ org.talend.mdm.core.storage.sql/src/test/java/com/amalto/core/storage/hibernate/LiquibaseSchemaAdapterTest.java
@@ -58,7 +58,7 @@ public class LiquibaseSchemaAdapterTest {
         Set<FieldMetadata> databaseIndexedFields = new HashSet<FieldMetadata>();
 
         TableResolver tableResolver = new StorageTableResolver(databaseIndexedFields, ((RDBMSDataSource) dataSource.get(storage
-                .getType())).getNameMaxLength());
+                .getType())).getNameMaxLength(), false);
 
         SessionFactoryImplementor sessionFactoryImplementor = (SessionFactoryImplementor) storage.getCurrentSession()
                 .getSessionFactory();

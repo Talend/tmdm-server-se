6b49e10692919a1d7881770ff37962351819eb63
yjli-talend <yjli@talend.com>
2017-07-26 14:37:58 +0800

    TMDM-11281 Remove license code from web UI.

--- org.talend.mdm.webapp.welcomeportal/src/main/java/org/talend/mdm/webapp/welcomeportal/client/WelcomePortal.java
+++ org.talend.mdm.webapp.welcomeportal/src/main/java/org/talend/mdm/webapp/welcomeportal/client/WelcomePortal.java
@@ -41,8 +41,6 @@ public class WelcomePortal implements EntryPoint {
 
     public final static String BROWSECONTEXT = "browserecords", BROWSEAPP = "BrowseRecords"; //$NON-NLS-1$//$NON-NLS-2$
 
-    public final static String LICENSECONTEXT = "licensemanager", LICENSEAPP = "LicenseManager"; //$NON-NLS-1$ //$NON-NLS-2$
-
     public final static String JOURNALCONTEXT = "journal", JOURNALAPP = "Journal"; //$NON-NLS-1$ //$NON-NLS-2$
 
     public final static String WORKFLOW_TASKCONTEXT = "workflowtasks", WORKFLOW_TASKAPP = "BonitaWorkflowTasks";//$NON-NLS-1$ //$NON-NLS-2$

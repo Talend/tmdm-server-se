a223e88792d320eb0d654d45b7780ab28f5d8cd3
yjli-talend <yjli@talend.com>
2017-11-03 16:38:09 +0800

    TMDM-11509 Improve code according to comment.

--- org.talend.mdm.core/src/com/amalto/core/util/UserHelper.java
+++ org.talend.mdm.core/src/com/amalto/core/util/UserHelper.java
@@ -27,11 +27,13 @@ public final class UserHelper {
     private UserHelper(boolean isFromRemote) {
         if (Util.isEnterprise()) {
             try {
-                Class<?> clazz = Class.forName("com.amalto.core.server.security.sso.MdmUserManageImpl"); //$NON-NLS-1$
+                Class<?> clazz = Class.forName("com.amalto.core.util.UserManageEnterpriseImpl"); //$NON-NLS-1$
                 Constructor<?> constructor = clazz.getConstructor(boolean.class);
                 um = (UserManage) constructor.newInstance(isFromRemote);
             } catch (Exception e) {
-                LOGGER.error("Could not create class 'com.amalto.core.server.security.sso.MdmUserManageImpl'.", e); //$NON-NLS-1$
+                String errorMessage = "Could not create class 'com.amalto.core.server.security.sso.MdmUserManageImpl'."; //$NON-NLS-1$
+                LOGGER.error(errorMessage);
+                throw new RuntimeException(errorMessage, e);
             }
         } else {
             um = new UserManageOptimizedImpl(isFromRemote);

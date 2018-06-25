1b2bb1d2686cea3e6adb49d5d10e15a58f29d4c3
yjli-talend <yjli@talend.com>
2018-06-25 14:20:32 +0800

    TMDM-12361 Export CSV file with reusable type data shows in the wrong place

--- org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/server/util/CSVWriter.java
+++ org.talend.mdm.webapp.browserecords/src/main/java/org/talend/mdm/webapp/browserecords/server/util/CSVWriter.java
@@ -21,6 +21,8 @@ public class CSVWriter extends DownloadWriter {
 
     private StringBuffer content = new StringBuffer();
 
+    private String[] lineDataArray;
+
     public CSVWriter(String concept, String viewPk, List<String> idsList, String[] headerArray, String[] xpathArray,
             String criteria, String multipleValueSeparator, String fkDisplay, boolean fkResovled, Map<String, String> colFkMap,
             Map<String, List<String>> fkMap, boolean isStaging, String language) {
@@ -40,14 +42,25 @@ public class CSVWriter extends DownloadWriter {
 
     @Override
     void generateLine() throws Exception {
+        lineDataArray = new String[headerArray.length];
         content.append(System.getProperty("line.separator")); //$NON-NLS-1$
     }
 
     @Override
     public void writeValue(String value) {
-        content.append(encodeValue(value));
-        if (columnIndex < headerArray.length - 1) {
-            content.append(","); //$NON-NLS-1$
+        lineDataArray[columnIndex] = encodeValue(value);
+        if (columnIndex == lineDataArray.length - 1) {
+            writeLine();
+        }
+    }
+
+    private void writeLine() {
+        for (int i = 0; i < lineDataArray.length; i++) {
+            String value = lineDataArray[i];
+            content.append(value == null ? "" : value);
+            if (i < lineDataArray.length - 1) {
+                content.append(","); //$NON-NLS-1$
+            }
         }
     }
 

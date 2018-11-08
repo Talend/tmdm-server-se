c37d8a80d5c73dd999b225023b5d7f4ac9037d42
Victor Li <vli@talend.com>
2018-11-08 17:07:37 +0800

    TMDM-12669 [REST API] Get the list of views
    Rebase from master.

--- org.talend.mdm.core/src/com/amalto/core/util/LocaleUtil.java
+++ org.talend.mdm.core/src/com/amalto/core/util/LocaleUtil.java
@@ -67,35 +67,6 @@ public class LocaleUtil {
         return locale;
     }
 
-    /**
-     * According to language to get corresponding value, 
-     * @param value, like "[fr:Produit avec Magasins][en:Product with Stores]"
-     * @param language, like "en", if language is empty, return raw value
-     * @return
-     * 
-     * Please note that function getValueByLanguage(in class org.talend.mdm.webapp.base.client.util.MultilanguageMessageParser) 
-     * has same functionality as below function getLocaleValue, They should be consistent during change.
-     */
-    public static String getLocaleValue(String value, String language) {
-        if (StringUtils.isEmpty(value)) {
-            return StringUtils.EMPTY;
-        }
-        if (StringUtils.isEmpty(language) || !LocaleUtils.isAvailableLocale(new Locale(language))) {
-            return value;
-        }
-
-        Pattern pattern = Pattern.compile("(?<=\\[)(.*?)(?=\\])"); //$NON-NLS-1$
-        Matcher matcher = pattern.matcher(value);
-        while (matcher.find()) {
-            String[] matcherPair = matcher.group().split(":", 2); //$NON-NLS-1$
-            if (matcherPair.length == 2 && language.equalsIgnoreCase(matcherPair[0])) {
-                return matcherPair[1];
-            }
-        }
-
-        return value;
-    }
-
     public static Locale getLocale(String language) {
         String localLanguage = language.toLowerCase();
         if (localLanguage.contains("_")) {

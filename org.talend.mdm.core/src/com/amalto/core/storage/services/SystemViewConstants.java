71782471c89d231fb03a7198692a287196a70ded
hw-zhu <hwzhu@talend.com>
2018-11-12 11:46:49 +0800

    TMDM-12670 [REST API] Get a view

---
+++ org.talend.mdm.core/src/com/amalto/core/storage/services/SystemViewConstants.java
@@ -0,0 +1,52 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package com.amalto.core.storage.services;
+
+public final class SystemViewConstants {
+
+    protected static final String NODE_NAME = "name";
+
+    protected static final String NODE_DESCRIPTION = "description";
+
+    protected static final String NODE_DATA_MODEL_ID = "dataModelId";
+
+    protected static final String NODE_ROLES = "roles";
+
+    protected static final String DEFAULT_VIEW_PREFIX = "Browse_items";
+
+    protected static final String DEFAULT_VIEW_ALL = "Browse_items_.*";
+
+    protected static final String SEARCHABLE_BUSINESS_ELEMENTS = "searchableBusinessElements";
+
+    protected static final String VIEWABLE_BUSINESS_ELEMENTS = "viewableBusinessElementsJson";
+
+    protected static final String WHERE_CONDITIONS = "whereConditions";
+
+    protected static final String SORT_FIELD = "sortField";
+
+    protected static final String CUSTOM_FORM = "customForm";
+
+    protected static final String IS_ASC = "isAsc";
+
+    protected static final String IS_TRANSFORMER_ACTIVE = "isTransformerActive";
+
+    protected static final String LEFT_PATH = "leftPath";
+
+    protected static final String OPERATOR = "operator";
+
+    protected static final String RIGHT_VALUE_OR_PATH = "rightValueOrPath";
+
+    protected static final String SPELL_CHECK = "spellCheck";
+
+    protected static final String STRING_PREDICATE = "stringPredicate";
+}

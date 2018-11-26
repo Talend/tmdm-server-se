b5079f3250a664d6621487ac9e0be95dbfb00f1a
htyin <htyin@talend.com>
2018-11-26 14:50:44 +0800

    TMDM-12625 [REST API] HTTP code 200 if data model doesn't exist

--- org.talend.mdm.core/src/com/amalto/core/storage/services/SystemModels.java
+++ org.talend.mdm.core/src/com/amalto/core/storage/services/SystemModels.java
@@ -33,6 +33,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;

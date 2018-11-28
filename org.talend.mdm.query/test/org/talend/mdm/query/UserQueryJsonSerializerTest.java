3d5922e232e927669cff829ea92a09d24fb9a7f7
Yohann Vinqueur <yvinqueur@talend.com>
2018-11-28 13:33:50 +0100

    TMDM-12897 Support predicate IN on JSONQuerySerializer

--- org.talend.mdm.query/test/org/talend/mdm/query/UserQueryJsonSerializerTest.java
+++ org.talend.mdm.query/test/org/talend/mdm/query/UserQueryJsonSerializerTest.java
@@ -19,8 +19,12 @@ import org.talend.mdm.QueryParserTest;
 import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
 import org.talend.mdm.commmon.metadata.MetadataRepository;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static com.amalto.core.query.user.UserQueryBuilder.and;
 import static com.amalto.core.query.user.UserQueryBuilder.eq;
+import static com.amalto.core.query.user.UserQueryBuilder.in;
 
 /**
  * Goal of unit test is pretty simple:
@@ -70,6 +74,18 @@ public class UserQueryJsonSerializerTest extends TestCase {
         final Select select = UserQueryBuilder.from(type1) //
                 .where(eq(type1.getField("id"), "1"))
                 .join(type1.getField("fk2"))
+                .getSelect();
+
+        // when, then
+        assertRoundTrip(select);
+    }
+
+    public void testUserQueryIn() {
+        List<String> ids = Arrays.asList("1", "2");
+        // given
+        final ComplexTypeMetadata type1 = repository.getComplexType("Type1");
+        final Select select = UserQueryBuilder.from(type1) //
+                .where(in(type1.getField("id"), ids))
                 .getSelect();
 
         // when, then

aca684207234506206ad49b5a571b52b752f4443
zfkang-talend <zfkang@talend.com>
2019-01-24 14:50:18 +0800

    TMDM-13063 Wrong order computation when importing records to server

--- org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/ReferenceEntityBridge.java
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/ReferenceEntityBridge.java
@@ -26,10 +26,10 @@ import com.amalto.core.storage.record.StorageConstants;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.lucene.document.Document;
+import org.hibernate.ObjectNotFoundException;
 import org.hibernate.search.bridge.LuceneOptions;
 import org.hibernate.search.bridge.TwoWayFieldBridge;
 
-import com.amalto.core.storage.Storage;
 import com.amalto.core.storage.hibernate.MultiLingualIndexedBridge.MultilingualIndexHandler;
 
 /**
@@ -145,8 +145,16 @@ public class ReferenceEntityBridge implements TwoWayFieldBridge {
                 if (value == null) {
                     break;
                 }
-                IndexHandler handler = getHandler(field, value);
-                handler.handle(name, value, field, document, luceneOptions);
+                try {
+                    if (value != null && value.toString() != null) {
+                        IndexHandler handler = getHandler(field, value);
+                        handler.handle(name, value, field, document, luceneOptions);
+                    }
+                } catch (ObjectNotFoundException e) {
+                    LOGGER.debug("Filed '" + field.getName() + "' is not persistent");
+                    ((Wrapper) dataObject).set(field.getName(), null);
+                    break;
+                }
             }
         }
     }

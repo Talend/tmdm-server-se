0d84b22768405d7a082337a8e77d76e2064290e8
zfkang-talend <zfkang@talend.com>
2019-04-16 16:19:13 +0800

    TMDM-13276 [Impact Analysis] ElementType change (length) not reflected in PostgreSQL database tables

--- org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/mapping/MDMTableUtils.java
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/mapping/MDMTableUtils.java
@@ -35,6 +35,9 @@ public abstract class MDMTableUtils {
         if (dialect instanceof PostgreSQLDialect) {
             return oldColumnInfo.getTypeName().toLowerCase().startsWith("varchar") && newColumn.getSqlType().equalsIgnoreCase("text");
         }
+        if (dialect instanceof SQLServerDialect) {
+            return isVarcharTypeChangedForMSSQL(newColumn, oldColumnInfo);
+        }
 
         return (oldColumnInfo.getTypeCode() == Types.VARCHAR || oldColumnInfo.getTypeCode() == Types.NVARCHAR) && (
                 newColumn.getSqlTypeCode() == Types.LONGVARCHAR || newColumn.getSqlTypeCode() == Types.CLOB);
@@ -52,12 +55,16 @@ public abstract class MDMTableUtils {
 
     private static boolean isIncreaseVarcharColumnLength(Column newColumn, ColumnMetadata oldColumnInfo, Dialect dialect) {
         if (dialect instanceof SQLServerDialect) {
-            return newColumn.getLength() > oldColumnInfo.getColumnSize() && (oldColumnInfo.getTypeCode() == Types.NVARCHAR
-                    || oldColumnInfo.getTypeCode() == Types.VARCHAR) && (newColumn.getSqlTypeCode() == Types.NVARCHAR
-                    || newColumn.getSqlTypeCode() == Types.VARCHAR);
+            return newColumn.getLength() > oldColumnInfo.getColumnSize() && isVarcharTypeChangedForMSSQL(newColumn,
+                    oldColumnInfo);
         }
         return newColumn.getLength() > oldColumnInfo.getColumnSize() && (newColumn.getSqlTypeCode() == oldColumnInfo
                 .getTypeCode());
+    }
+
+    private static boolean isVarcharTypeChangedForMSSQL(Column newColumn, ColumnMetadata oldColumnInfo) {
+        return (oldColumnInfo.getTypeCode() == Types.NVARCHAR || oldColumnInfo.getTypeCode() == Types.VARCHAR) && (
+                newColumn.getSqlTypeCode() == Types.NVARCHAR || newColumn.getSqlTypeCode() == Types.VARCHAR);
     }
 
     public static boolean isChangedToOptional(Column newColumn, ColumnMetadata oldColumnInfo) {

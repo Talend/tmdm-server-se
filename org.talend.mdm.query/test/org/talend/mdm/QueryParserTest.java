d452ca0669ffe4fb93f5aaa62b9a485627fb9d57
zfkang-talend <zfkang@talend.com>
2019-04-24 16:16:32 +0800

    TMDM-13371 MDM REST API PUT /data/{containerName}/query : select where eq does not work when non ascii characters

--- org.talend.mdm.query/test/org/talend/mdm/QueryParserTest.java
+++ org.talend.mdm.query/test/org/talend/mdm/QueryParserTest.java
@@ -511,7 +511,7 @@ public class QueryParserTest extends TestCase {
     //Test non ascii characters
     public void testQuery32() {
         QueryParser parser = QueryParser.newParser(repository);
-        Expression expression = parser.parse(QueryParserTest.class.getResourceAsStream("query32.json")); //$NON-NLS-1$
+        Expression expression = parser.parse(QueryParserTest.class.getResourceAsStream("query32_1.json")); //$NON-NLS-1$
         assertTrue(expression instanceof Select);
         Select select = (Select) expression;
 
@@ -520,6 +520,28 @@ public class QueryParserTest extends TestCase {
         assertTrue(condition instanceof Compare);
         Compare compare = (Compare)condition;
         assertEquals("æ", ((StringConstant)compare.getRight()).getValue());
+
+        parser = QueryParser.newParser(repository);
+        expression = parser.parse(QueryParserTest.class.getResourceAsStream("query32_2.json")); //$NON-NLS-1$
+        assertTrue(expression instanceof Select);
+        select = (Select) expression;
+
+        condition = select.getCondition();
+        assertNotNull(condition);
+        assertTrue(condition instanceof Compare);
+        compare = (Compare)condition;
+        assertEquals("我是中国人", ((StringConstant)compare.getRight()).getValue());
+
+        parser = QueryParser.newParser(repository);
+        expression = parser.parse(QueryParserTest.class.getResourceAsStream("query32_3.json")); //$NON-NLS-1$
+        assertTrue(expression instanceof Select);
+        select = (Select) expression;
+
+        condition = select.getCondition();
+        assertNotNull(condition);
+        assertTrue(condition instanceof Compare);
+        compare = (Compare)condition;
+        assertEquals("æœøÆŒØï", ((StringConstant)compare.getRight()).getValue());
     }
 
 }

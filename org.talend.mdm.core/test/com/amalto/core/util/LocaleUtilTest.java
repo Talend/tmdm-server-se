0d3fc89e9a6f7d8a58cc278aa00cf451be479155
Victor Li <vli@talend.com>
2019-09-16 10:39:27 +0800

    TMDM-13940 [REST API] The name of a view is not well return on API

--- org.talend.mdm.core/test/com/amalto/core/util/LocaleUtilTest.java
+++ org.talend.mdm.core/test/com/amalto/core/util/LocaleUtilTest.java
@@ -12,45 +12,45 @@ package com.amalto.core.util;
 import junit.framework.TestCase;
 
 @SuppressWarnings("nls")
-public class MultilanguageMessageParserTest extends TestCase {
+public class LocaleUtilTest extends TestCase {
 
     public void testNormalLocaleValue() {
         String str1 = "[FR:Produit avec Magasins][EN:Product with Stores][ZH:Zhong Wen]";
 
         //case 1
-        String results = MultilanguageMessageParser.getValueByLanguage(str1, "en");
+        String results = LocaleUtil.getLocaleValue(str1, "en");
         assertEquals("Product with Stores", results);
 
         //case 2
-        results = MultilanguageMessageParser.getValueByLanguage(str1, "FR");
+        results = LocaleUtil.getLocaleValue(str1, "FR");
         assertEquals("Produit avec Magasins", results);
 
         //case 3
-        results = MultilanguageMessageParser.getValueByLanguage(str1, "zh");
+        results = LocaleUtil.getLocaleValue(str1, "zh");
         assertEquals("Zhong Wen", results);
 
         //case 4
-        results = MultilanguageMessageParser.getValueByLanguage(str1, "zh_CN");
+        results = LocaleUtil.getLocaleValue(str1, "zh_CN");
         //assertEquals(str1, results);
 
         //case 5
-        results = MultilanguageMessageParser.getValueByLanguage(str1, "");
+        results = LocaleUtil.getLocaleValue(str1, "");
         assertEquals(str1, results);
 
         //case 6
-        results = MultilanguageMessageParser.getValueByLanguage(str1, "xx");
+        results = LocaleUtil.getLocaleValue(str1, "xx");
         //assertEquals(str1, results);
 
         //case 7
-        results = MultilanguageMessageParser.getValueByLanguage("", "en");
+        results = LocaleUtil.getLocaleValue("", "en");
         assertEquals("", results);
 
         //case 8
-        results = MultilanguageMessageParser.getValueByLanguage("", "");
+        results = LocaleUtil.getLocaleValue("", "");
         assertEquals("", results);
 
         //case 9
-        results = MultilanguageMessageParser.getValueByLanguage("", "xx");
+        results = LocaleUtil.getLocaleValue("", "xx");
         assertEquals("", results);
     }
 
@@ -58,15 +58,15 @@ public class MultilanguageMessageParserTest extends TestCase {
         String str1 = "[FR:[Org\\]Produit avec Magasins][EN:[Org\\]Product with Stores][ZH:Zhong Wen]";
 
         //case 1
-        String results = MultilanguageMessageParser.getValueByLanguage(str1, "EN");
+        String results = LocaleUtil.getLocaleValue(str1, "EN");
         assertEquals("[Org]Product with Stores", results);
 
         //case 2
-        results = MultilanguageMessageParser.getValueByLanguage(str1, "FR");
+        results = LocaleUtil.getLocaleValue(str1, "FR");
         assertEquals("[Org]Produit avec Magasins", results);
 
         //case 3
-        results = MultilanguageMessageParser.getValueByLanguage(str1, "zh");
+        results = LocaleUtil.getLocaleValue(str1, "zh");
         assertEquals("Zhong Wen", results);
     }
 }

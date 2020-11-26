9fc4883bbc42e10882dc5fd07230031fd84002f3
htyin <htyin@talend.com>
2020-11-26 14:51:59 +0800

    TMDM-14613 Failed to run hierarchy to view records using customer's datamodel

--- org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/StandardQueryHandler.java
+++ org.talend.mdm.core.storage.sql/src/main/java/com/amalto/core/storage/hibernate/StandardQueryHandler.java
@@ -1828,10 +1828,9 @@ class StandardQueryHandler extends AbstractQueryHandler {
         boolean isReferenceField = fieldMetadata instanceof ReferenceFieldMetadata;
         boolean isContainedInMain = mainType.equals(fieldMetadata.getContainingType());
         boolean isSelfReference = isSelfReference(fieldMetadata);
-        boolean isMany = fieldMetadata.isMany();
         if (isCompoundField) {
             addConditionForCompoundField(condition, alias, fieldMetadata);
-        } else if (isReferenceField && isSelfReference && !isMany) {
+        } else if (isReferenceField && isSelfReference && !fieldMetadata.isMany()) {
             addConditionForSelfReferenceField(condition, alias, ((ReferenceFieldMetadata) fieldMetadata).getReferencedField());
         } else if (isReferenceField && isContainedInMain) {
             condition.criterionFieldNames.add(getFieldName(fieldMetadata, true));

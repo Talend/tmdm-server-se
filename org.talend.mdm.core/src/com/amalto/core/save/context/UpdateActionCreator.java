b91bcdaa54d7d1232e4bdc0d75c5dde18c480c06
hwzhu <hwzhu@talend.com>
2021-02-20 16:06:18 +0800

    TMDM-10143 Can't save complex sub-element to empty value

--- org.talend.mdm.core/src/com/amalto/core/save/context/UpdateActionCreator.java
+++ org.talend.mdm.core/src/com/amalto/core/save/context/UpdateActionCreator.java
@@ -26,8 +26,8 @@ import java.util.Stack;
 import java.util.UUID;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.talend.mdm.commmon.metadata.ComplexTypeMetadata;
 import org.talend.mdm.commmon.metadata.ContainedTypeFieldMetadata;
 import org.talend.mdm.commmon.metadata.DefaultMetadataVisitor;
@@ -533,7 +533,8 @@ public class UpdateActionCreator extends DefaultMetadataVisitor<List<Action>> {
                     ComplexTypeMetadata previousTypeMetadata = (ComplexTypeMetadata) repository.getNonInstantiableType(repository.getUserNamespace(), previousType);
                     if (!previousTypeMetadata.getSuperTypes().isEmpty() || !previousTypeMetadata.getSubTypes().isEmpty()) {
                         if (rightAccessor.exist() && rightAccessor.getActualType().isEmpty()
-                                && rightAccessor.getClass().getName().equals("com.amalto.core.history.accessor.UnaryFieldAccessor")) {
+                                && rightAccessor.getClass().getName().equals("com.amalto.core.history.accessor.UnaryFieldAccessor")
+                                && actions.size() == 0) {
                             actions.add(new FieldUpdateAction(date, source, userName, getLeftPath(), previousType, null, field, userAction));
                         } else if (!rightAccessor.exist()) {
                             actions.addAll(ChangeTypeAction.create(originalDocument, date, source, userName, getLeftPath(), previousTypeMetadata, null, field, userAction));
